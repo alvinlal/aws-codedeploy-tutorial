8efd208b8fa518f463e12b7447ab9e9676df8e4f
alvinlal <alvinzzz2001@gmail.com>
2023-01-24 17:20:10 +0530

    add shebang

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 cd /home/ec2-user/codedeploy-tutorial && npm install --omit=dev && npm run build
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 pm2 restart /home/ec2-user/aws-codedeploy-tutorial/codedeploy-tutorial.pm2.json
